5830b14e5402749f0954bb66df470db4bf545938
Dirosaki <diegosgomes27@gmail.com>
2021-08-17 14:51:04 -0300

    Mascáras de input

---
+++ src/utils/masks.ts
@@ -0,0 +1,17 @@
+export const masks = {
+  cpf(value: string) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  },
+  date(value: string) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "$1/$2")
+      .replace(/(\d{2})(\d)/, "$1/$2")
+      .replace(/(\d{4})\d+?$/, "$1");
+  }
+};
